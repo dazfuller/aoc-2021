e8b353106bc72876be8669ec4d5f329eaa727338
Darren Fuller <daz.fuller@gmail.com>
2021-12-04 11:34:13 +0000

    switched to using Source.fromResource

--- src/main/scala/com/github/dazfuller/aoc2021/Utils.scala
+++ src/main/scala/com/github/dazfuller/aoc2021/Utils.scala
@@ -6,9 +6,7 @@ object Utils {
   def getInputData(day: Int, isTest: Boolean = false): List[String] = {
     val dayDir = f"day${day.toString.padTo(2, '0').reverse}"
     val filePart = s"${if (isTest) "test_" else ""}input.txt"
-    val inputPath = getClass.getResource(s"/$dayDir/$filePart").getPath
-    val source = Source.fromFile(inputPath)
 
-    source.getLines().toList
+    Source.fromResource(s"$dayDir/$filePart").getLines().toList
   }
 }
